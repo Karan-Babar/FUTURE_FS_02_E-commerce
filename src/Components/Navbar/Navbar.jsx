f99fb8c741068cca002194bafb83c53a92d3794e
Karan Babar <karan.babar2004@gmail.com>
2025-12-05 13:22:10 +0530

    Added Search box in the navbar component and also change the hero section of website.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -29,6 +29,15 @@ const Navbar = () => {
         <li onClick={() => {setMenu("womens")}}><Link style={{textDecoration: 'none'}} to="/womens">Women</Link>{menu==="womens"?<hr />:<></>}</li>
         <li onClick={() => {setMenu("kids")}}><Link style={{textDecoration: 'none'}} to="/kids">Kids</Link>{menu==="kids"?<hr />:<></>}</li>
       </ul>
+       <div className="nav-search">
+            <select className="search-select">
+               <option>ALL</option>
+            </select>
+            <input placeholder="Search Items" className="search-input"/>
+            <div className="search-icon">
+               <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+           </div>
       <div className="nav-login-cart">
         <Link style={{textDecoration: 'none'}} to="/login"><button>Login</button></Link>
         <Link style={{textDecoration: 'none'}} to="/cart"><img src={cart_icon} alt='' /></Link>
